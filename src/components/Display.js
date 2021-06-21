063b6aa109bff9666beae5bc22352d9d25ed7582
jayaramsivaramannair <jayaramsivaramannair@gmail.com>
2021-06-20 22:11:05 -0400

    Added some hover effect to each tab displaying character stats

--- src/components/Display.js
+++ src/components/Display.js
@@ -6,10 +6,14 @@ import Profile from './Profile.js'
 import { ProfilesContext } from '../App.js'
 
 export default function Display() {
+
     const profilesFromContext = useContext(ProfilesContext)
+
     const [currentProfile, setCurrentProfile] = useState(0)
     const profileLength = profilesFromContext.profiles.profiles.length
     const allProfiles = profilesFromContext.profiles.profiles
+    console.log(profilesFromContext)
+
 
 
     if (!Array.isArray(allProfiles) || allProfiles.length <= 0) {
@@ -17,14 +21,15 @@ export default function Display() {
     }
 
 
-    /* To be attached to the right arrow*/
     const nextProfile = () => {
         setCurrentProfile(currentProfile === profileLength - 1 ? 0 : currentProfile + 1)
     }
 
+
     const previousProfile = () => {
         setCurrentProfile(currentProfile === 0 ? profileLength - 1 : currentProfile - 1)
     }
+
 
     const character = allProfiles.filter((profile, index) => {
         let newProfile = ''
