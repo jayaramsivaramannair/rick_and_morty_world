9e534176fa5df4c121ef7edea1ebfbf00337ea78
Jayaram Nair <jayaramsivaramannair@gmail.com>
2021-09-22 20:47:47 -0400

    Added styling to make the app occupy full screen height

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,6 +3,7 @@ import React from 'react'
 export default function Profile({ character }) {
     return (
         <div className="profile">
+            <h2>{`The Who, the What, and the When: ${character.name}`}</h2>
             <div className="tab tab-1"><span className="label-1">Gender:</span> {character.gender}</div>
             <div className="tab tab-2"><span className="label-2">Species:</span> {character.species}</div>
             <div className="tab tab-3"><span className="label-3">Location:</span> {character.location.name}</div>
